46e3474f18793466cf9ddfb12ae62b45a1e78df8
Matty <math249e@easv365.dk>
2024-12-02 14:56:12 +0100

    Now able to find the top 5 gainers

--- src/TheRedAgent/testAPI.py
+++ src/TheRedAgent/testAPI.py
@@ -1,11 +1,21 @@
 from FinAPIWrapper import FinancialModelingPrepAPI
 
 # Replace before push
-api = FinancialModelingPrepAPI(api_key="random")
+api = FinancialModelingPrepAPI(api_key="Random")
 
+'''
 query = "Apple"
 result = api.search_company(query)
 if result:
     print("Search Results:", result)
 else:
     print("No results or an error occurred.")
+
+'''
+
+top_gainers = api.get_top_gainers()
+if top_gainers:
+    for i, gainer in enumerate(top_gainers, 1):
+        print(f"{i}. {gainer['name']}: {gainer['changesPercentage']}%")
+else:
+    print("No data found or an error occurred.")
